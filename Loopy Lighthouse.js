c451d35985f8d94a3aa47d5f121bd97f39b4bb10
tofi-124 <tofikahm@yahoo.com>
2021-10-12 19:38:36 -0400

    Evaluation problem solutions

---
+++ Loopy Lighthouse.js
@@ -0,0 +1,19 @@
+var i;
+for(i = 100;i < 201;i++){
+  
+  if (i % 3 === 0 && i % 4 === 0){
+    console.log("LoopyLighthouse");
+  }
+  
+  else  if (i % 3 === 0){
+    console.log("Loopy");
+  }
+  
+  else if(i % 4 === 0){
+    console.log("Lighthouse");
+  }
+
+  else{
+    console.log(i);
+  }
+}
