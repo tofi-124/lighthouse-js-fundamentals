8853c4a3ba626e9e81ae3be0a2da00ec00d27997
tofi-124 <tofikahm@yahoo.com>
2021-10-13 16:13:53 -0400

    lastIndexOf Assignment

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,10 +1,19 @@
-function lastIndexOf(index=[],of){
-  i=0
-  while(i<index.length)
-  {
+function lastIndexOf(index = [ ] , of){
+  var i = 0
+  var pl = -1
+  while(i < index.length)
+  { 
+    if(of === index[i]){
+      pl = i
+    }
     i++
   }
-  return i;
+  return pl
 }
 
-console.log(lastIndexOf([0,1,2]))
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
