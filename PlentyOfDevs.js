bd7c9f970b5c286f73a5a590d012838b07312710
tofi-124 <tofikahm@yahoo.com>
2021-10-13 05:43:52 +0000

    Functions - Age calculator

---
+++ PlentyOfDevs.js
@@ -0,0 +1,10 @@
+function ageCalculator(name,yearOfBirth,currentYear){
+  let n = name;
+  let yob = yearOfBirth;
+  let cyr = currentYear;
+  
+  return n + " is " + (cyr-yob) + " years old."
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
