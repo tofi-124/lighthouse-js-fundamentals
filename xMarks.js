fecf083c624f2a2df4e584cd3a36749f7b91456c
tofi-124 <tofikahm@yahoo.com>
2021-10-13 05:37:10 +0000

    X Marks the Perfect Shot

---
+++ xMarks.js
@@ -0,0 +1,24 @@
+const finalPosition = function (moves = []) {
+  var x = 0
+  var y = 0
+
+  for(var i = 0;i < moves.length;i++){
+    if(moves[i] === 'north'){
+      y++
+    }
+    else if(moves[i] === 'south'){
+      y--
+    }
+    else if(moves[i] === 'west'){
+      x--
+    }
+    else if(moves[i] === 'east'){
+      x++
+    }
+  }
+  return [x,y]
+}
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+finalPosition(moves);
