af9e43e17e7e089b0e57feb59e1f275d063e34f7
tofi-124 <tofikahm@yahoo.com>
2021-10-13 16:13:35 -0400

    Concatenate Arrays Assignment

---
+++ concat.js
@@ -0,0 +1,20 @@
+function concat(firstArray = [] , secondArray = []) {
+  var con = []
+  var j = 0
+
+  while(j < firstArray.length){
+    con[j] = firstArray[j]
+    j++
+  }
+
+  j = 0
+  while(j < secondArray.length){
+    con.push(secondArray[j])
+    j++
+  }
+
+  return con
+}
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]))
