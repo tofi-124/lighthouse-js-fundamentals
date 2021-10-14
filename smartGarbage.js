a32f736e303d2db6bc8d6698a0596e605e5c4c51
tofi-124 <tofikahm@yahoo.com>
2021-10-14 13:20:39 -0400

    assigment smartGarbage.js

---
+++ smartGarbage.js
@@ -0,0 +1,8 @@
+let waste, recycling, compost
+function smartGarbage(trash,bins = { waste, recycling, compost }) {
+  
+  if (trash === 'waste') bins.waste++;
+  else if (trash === 'recycling') bins.recycling++;
+  else if (trash === 'compost') bins.compost++;
+  return bins
+}
